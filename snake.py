7707ab0353f8a1723858e29f707bb14eb21f7731
aritra-b-11 <aritrabhattacharjee12@gmail.com>
2017-12-16 01:00:32 +0530

    score added

--- snake.py
+++ snake.py
@@ -1,14 +1,15 @@
 import pygame, sys
 from pygame.locals import *
 from random import randint
+from random import choice
 
 pygame.init()
-pixel = 30
+pixel = 20
 width_mult, height_mult = 40, 20
 width, height = pixel*width_mult, pixel*height_mult
 
+myfont = pygame.font.SysFont("monospace", 16)
 pygame.display.set_caption("Snake")
-
 
 white = (255,255,255)
 black = (0,0,0)
@@ -51,6 +52,8 @@ score = 0
 
 length = 3
 direction_array=[]
+position_array_X = []
+position_array_Y = []
 
 for i in range(length-1):
     direction_array.append(direction)
@@ -67,33 +70,39 @@ while True:
 #    print snakeHeadX,snakeHeadY
     if(direction == 'right'):
         snakeHeadX += speed
-        if(snakeHeadX >= width-pixel):
+        if(snakeHeadX > width-pixel):
             snakeHeadX = pixel
+            #print "right"
     elif(direction == 'left'):
         snakeHeadX -= speed
-        if(snakeHeadX <= pixel):
+        if(snakeHeadX < pixel):
             snakeHeadX = width-pixel
+            #print "left"
     elif(direction == 'up'):
         snakeHeadY -=speed
-        if(snakeHeadY <= pixel):
+        if(snakeHeadY < pixel):
             snakeHeadY = height-pixel
+            #print "up"
     elif(direction == 'down'):
         snakeHeadY += speed
-        if(snakeHeadY >= height-pixel):
+        if(snakeHeadY > height-pixel):
             snakeHeadY = pixel
+            #print "down"
     DISPLAYSURF.blit(snakeHeadImg, (snakeHeadX,snakeHeadY))
     DISPLAYSURF.blit(geccoImg, (geccoX,geccoY))
     if(snakeHeadX==geccoX and snakeHeadY==geccoY):
         length += 1
         score += 1
         direction_array.append(direction)
-        geccoX= randint(2,width_mult-2)*pixel
-        geccoY= randint(2,height_mult-2)*pixel
+        geccoX= choice([i for i in range(1,width_mult-1) if i*pixel not in position_array_X])*pixel
+        geccoY= choice([i for i in range(1,height_mult-1) if i*pixel not in position_array_Y])*pixel
         # chose a rand number excluding the position array
-#        print geccoY,geccoX
+        # print geccoY,geccoX
     pivotX=snakeHeadX
     pivotY=snakeHeadY
-
+    position_array_X = []
+    position_array_Y = []
+    # print geccoX,geccoY
     for i in range(length-1):
         if(direction_array[i]=='right'):
             pivotX -= pixel
@@ -103,9 +112,11 @@ while True:
             pivotY += pixel
         elif(direction_array[i]=='down'):
             pivotY -= pixel
-        position_array.append((pivotX,pivotY))
+        position_array_X.append(pivotX)
+        position_array_Y.append(pivotY)
         DISPLAYSURF.blit(snakeBodyImg, (pivotX,pivotY))
-#    print "position", position_array
+        # print "position", position_array
+    #print snakeHeadX,snakeHeadY
     for event in pygame.event.get():
         if event.type==QUIT:
             pygame.quit()
@@ -123,11 +134,28 @@ while True:
             elif event.key==pygame.K_DOWN:
                 direction = 'down'
                 snakeHeadImg = snakeHeadImgDown
+            elif event.key==pygame.K_ESCAPE:
+                paused(width,height,DISPLAYSURF)
+    scoretext = myfont.render("Score {0}".format(score), 1, (0,0,0))
+    DISPLAYSURF.blit(scoretext, (5, 5))
     direction_array=direction_array[::-1]
     direction_array.append(direction)
     direction_array=direction_array[::-1]
     del(direction_array[len(direction_array)-1])
     pygame.display.update()
     fpsClock.tick(FPS)
+
+
+def paused(x,y,gameDisplay):
+    textPaused = "Paused"
+    textRect.center = ((x/2),(y/2))
+    gameDisplay.blit(textPaused,textRect)
+
+    for event in pygame.event.get():
+        if event.type==QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.key==pygame.K_ESCAPE:
+            paused(width,height,DISPLAYSURF)
 
 
